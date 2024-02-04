6ac0a04f4038378facde7c2e1f41b0ca5b26cba6
Daniel Hasdiel <danielhasdiel@gmail.com>
2024-02-04 22:41:32 +0200

    add example and the ruler to thecomponent
    nedd to fix the imports of the leaflet-ruler files

---
+++ example/src/App.tsx
@@ -0,0 +1,27 @@
+import Ruler from "react-leaflet-ruler";
+import "./App.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+function App() {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: "100vh", width: "100wh" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Ruler />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default App;
--- src/Ruler.tsx
+++ src/Ruler.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./leaflet-ruler";
+import "./leaflet-ruler.css";
+import { useMap } from "react-leaflet";
+import L from "leaflet";
+
+interface LeafletRuler extends L.Control {
+  _allLayers: {};
+  _choice: boolean;
+  _defaultCursor: string;
+  _container: string;
+  _initHooksCalled: boolean;
+  _lastClickTime: string;
+  _leaflet_id: number;
+  _map: L.Map | ExtendMap;
+  _toggleMeasure: () => void;
+}
+
+interface ExtendMap extends L.Map {
+  ruler?: LeafletRuler;
+}
 
 const Ruler = () => {
-  return <div>ruler</div>;
+  const map: ExtendMap = useMap();
+
+  useEffect(() => {
+    if (!map) return;
+    (L.control as any).ruler().addTo(map);
+  }, []);
+
+  return null;
 };
 
 export default Ruler;
