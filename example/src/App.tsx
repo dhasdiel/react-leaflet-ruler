d5fa72cf724485a13bc4ac1b30d5ba4f58d429bc
Daniel Hasdiel <danielhasdiel@gmail.com>
2024-02-04 23:39:23 +0200

    i leaflet types

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -10,10 +10,7 @@ function App() {
       scrollWheelZoom={true}
       className="map"
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Ruler />
       <Marker position={[51.505, -0.09]}>
         <Popup>
